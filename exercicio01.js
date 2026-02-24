a3c70fd65a622b96c1eb7d2934e86dc8db4dabbb
samuel-assuncao-nave <samuel.assuncao@nave.org.br>
2026-02-24 14:40:51 -0300

    respondido

--- exercicio01.js
+++ exercicio01.js
@@ -34,16 +34,16 @@
  * as boas práticas de nomeação para variáveis (e não liguem para o "export" na frente. Mantenham ele):
  */
 // Resposta
-/* 01 */ export let meu-nome = "Felizberto Adalberto"
-/* 02 */ export const g@toRajado = "Kiki"
-/* 03 */ export let quantidade_de_bolos = 75
+/* 01 */ export let meunome = "Felizberto Adalberto"
+/* 02 */ export const gatoRajado = "Kiki"
+/* 03 */ export let QUANTIDADE_DE_BOLOS = 75
 /* 04 */ export let MeusColegas = ["Fulado", "Ciclano", "Beltrano"]
 /* 05 */ export const condução = "Ônibus"
-/* 06 */ export class alunoDoNave { constructor() {} }
+/* 06 */ export class AlunoDoNave { constructor() {} }
 /* 07 */ export let GolpeDoGoku = "Kamehameha"
-/* 08 */ export const tom&Jerry = "Tom e Jerry"
-/* 09 */ export const Eh_sobre_isso = "E tá tudo bem"
-/* 10 */ export let meu-endereço = { rua: "Avenida sei lá", numero: 123, bairro: "Rouxinol Goiaba"}
+/* 08 */ export const tom_Jerry = "Tom e Jerry"
+/* 09 */ export const eh_sobre_isso = "E tá tudo bem"
+/* 10 */ export let meuendereço = { rua: "Avenida sei lá", numero: 123, bairro: "Rouxinol Goiaba"}
 /* 11 */ export class OperaçãoExplosiva { constructor () {} }
-/* 12 */ export class Personagem-Fictício { constructor() {} }
+/* 12 */ export class PersonagemFictício { constructor() {} }
 
