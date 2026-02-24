b4cab8345b27dac5cf2c1b070f1a01bf7927b428
Arthur <arthur.machado@nave.org.br>
2026-02-24 14:24:48 -0300

    Respondido os exercicios 1 e 2

--- exercicio01.js
+++ exercicio01.js
@@ -23,7 +23,7 @@
  * 4. Nomes compostos são escritos em "camelCase", exemplo: victorMatheusCavalcanti
  * 5. Nomes de classes devem começar com letra maiúscula
  * 6. Palavras que já são parte da sintaxe do JavaScript não podem ser utilizados como nomes de 
- *    variáveis, eemplo: "let else" <- Errado, else já é uma palavra da sintaxe "if { } else { }"
+ *    variáveis, exemplo: "let else" <- Errado, else já é uma palavra da sintaxe "if { } else { }"
  * 
  * Agora responda as atividades abaixo
  */
@@ -34,16 +34,16 @@
  * as boas práticas de nomeação para variáveis (e não liguem para o "export" na frente. Mantenham ele):
  */
 // Resposta
-/* 01 */ export let meu-nome = "Felizberto Adalberto"
-/* 02 */ export const g@toRajado = "Kiki"
-/* 03 */ export let quantidade_de_bolos = 75
-/* 04 */ export let MeusColegas = ["Fulado", "Ciclano", "Beltrano"]
-/* 05 */ export const condução = "Ônibus"
-/* 06 */ export class alunoDoNave { constructor() {} }
-/* 07 */ export let GolpeDoGoku = "Kamehameha"
-/* 08 */ export const tom&Jerry = "Tom e Jerry"
-/* 09 */ export const Eh_sobre_isso = "E tá tudo bem"
-/* 10 */ export let meu-endereço = { rua: "Avenida sei lá", numero: 123, bairro: "Rouxinol Goiaba"}
-/* 11 */ export class OperaçãoExplosiva { constructor () {} }
-/* 12 */ export class Personagem-Fictício { constructor() {} }
+/* 01 */ export let meuNome = "Felizberto Adalberto"
+/* 02 */ export const gatoRajado = "Kiki"
+/* 03 */ export let quantidadeDeBolos = 75
+/* 04 */ export let meusColegas = ["Fulado", "Ciclano", "Beltrano"]
+/* 05 */ export const conducao = "Ônibus"
+/* 06 */ export class AlunoDoNave { constructor() {} }
+/* 07 */ export let golpeDoGoku = "Kamehameha"
+/* 08 */ export const tomEJerry = "Tom e Jerry"
+/* 09 */ export const ehSobreIsso = "E tá tudo bem"
+/* 10 */ export let meuEndereco = { rua: "Avenida sei lá", numero: 123, bairro: "Rouxinol Goiaba"}
+/* 11 */ export class OperacaoExplosiva { constructor () {} }
+/* 12 */ export class PersonagemFicticio { constructor() {} }
 
